0f46b426c47a5056b6e28707a87ad712770b6759
vindartproject <nathaliaperezquintero456@gmail.com>
2022-10-25 06:16:44 -0500

    cambios carro compras

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const items = document.getElementById('items')
 const templateCard = document.getElementById('template-card').content
 const fragment = document.createDocumentFragment()
+let carrito = {}
 
 document.addEventListener('DOMContentLoaded', () => {
     fetchData()
+})
+
+items.addEventListener('click', e =>{
+    addCarrito(e)
 })
 
 const fetchData = async() => {
@@ -23,9 +28,27 @@ const pintarCards = data => {
          templateCard.querySelector('img').setAttribute("src", producto.thumbnailUrl)
          templateCard.querySelector('h5').textContent = producto.nombre
          templateCard.querySelector('p').textContent = producto.precio
-
+         templateCard.querySelector('button').dataset.id = producto.id
+         
          const clone =templateCard.cloneNode(true)
          fragment.appendChild(clone)
     })
     items.appendChild(fragment)
+}
+
+const addCarrito = e => {
+//    console.log(e.target)
+//    console.log()
+    if(e.target.classList.contains('button')){
+        setCarrito(e.target.parentElement)
+    }
+    e.stopPropagation()
+}
+
+const setCarrito = objeto => {
+    // console.log(objeto)
+    const producto = {
+        id: objeto.querySelector('button').dataset.id
+    }
+    console.log(producto)
 }
