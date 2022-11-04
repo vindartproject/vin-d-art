cdb5c4b15d753d8c9699ebab2f89da64d34a7d41
Karoh-Heta <henaotocariak@gmail.com>
2022-11-04 12:56:28 -0500

    stripe

---
+++ assets/js/Keys.js
@@ -0,0 +1,4 @@
+export default {
+    public:"pk_live_51LoDgpDQHa1SORpUNWNpPUWcbWTUouX4IS9sn9O2ucfcPu7XSWW6GynXAWRv2YZiuv24C7hb0M6ZDPr8hwPwv6Hc009AbznQ1R",
+    secret:"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"
+}
